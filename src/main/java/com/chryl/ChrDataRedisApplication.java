a5b18cd06e216afe1cf763e189fc50b3183d16a6
Chr.yl <cyl5yy412@gmail.com>
2020-04-16 15:54:03 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrDataRedisApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrDataRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrDataRedisApplication.class, args);
+	}
+
+}
